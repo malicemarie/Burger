13b19a8116f85cfda0f5312e7b23737d422d0139
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-02-26 13:54:50 -0800

    updated schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Crispy Chicken Sandwich", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
